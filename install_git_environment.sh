b153ebc86bb67992b2cd4dff74e3cb16ac8eff9d
Jerome <minghui.jiang.sh@gmail.com>
2020-09-30 20:14:24 +0300

    Description: Modify git editor to vim Author: Jerome Email: minghui.jiang.sh@gmail.com

--- install_git_environment.sh
+++ install_git_environment.sh
@@ -4,6 +4,7 @@ LOCAL_PATH=`pwd`
 cp "$LOCAL_PATH"/gitmsg ~/.gitmsg -f
 git config --global user.name "Jerome"
 git config --global user.email "minghui.jiang.sh@gmail.com"
+git config --global core.editor "vim"
 git config --global commit.template ~/.gitmsg
 
 echo "git environment set successfully!"
